055bbdd5ebce2b6e9c94ad1f07ab2afe7d563d73
Daniel de Oliveira <127465821+HxHayato@users.noreply.github.com>
2023-05-28 19:30:31 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,70 +1,91 @@
+/*
+    Fluxo
+
+    1º Carrega a página e cria as opções do select lendo as tabelas de "roletas.json" existentes
+
+    2º Quando o usuário pede as raças, ele sorteia e depois mostra o resultado, agrupando as raças iguais e colocando o número de vezes que apareceram
+*/
+
 //Ao carregar a página
 document.addEventListener('DOMContentLoaded', () => {
     criarOpcoesSelect()
 })
 
-//Abrir e fechar select
-let dropdownSelect = document.querySelector('.dropdown-select') 
-let select = document.querySelector('.select')
-let opcaoEscolhida = document.querySelector('.escolhido')
-let seta = document.querySelector('.bxs-down-arrow')
-let menuOpcoes = document.querySelector('.outras')
-let opcoes = document.querySelectorAll('.opcoes')
+//Lendo o JSON e criando as opções de roletas
+async function criarOpcoesSelect () {
+    try {
+        const roletas = await fetch('roletas.json')
+        const dados = await roletas.json()
 
-dropdownSelect.addEventListener('click', (e) => {
-    abrirOpcoes()
-})
+        for await (const objeto of dados){
+            criarEstruturaOption(objeto.regiao)
+        }
 
-function abrirOpcoes () {
-    if(seta.classList.contains('virar')){
-        //se já foi virada antes
-        seta.classList.remove('virar')
-        seta.classList.add('desvirar')
-
-        //Fecha as opções
-        menuOpcoes.classList.remove('aberto')
-        menuOpcoes.classList.add('fechado') 
-
-        dropdownSelect.style.borderRadius = '3px'
-    } else if (seta.classList.contains('desvirar')){
-        //se já foi desvirada antes
-        seta.classList.remove('desvirar')
-        seta.classList.add('virar')
-
-        //Abre as opções
-        menuOpcoes.classList.remove('fechado')
-        menuOpcoes.classList.add('aberto')
-
-        dropdownSelect.style.borderRadius = '3px 3px 0 0'
-    } else {
-        //se for a primeira vez
-        seta.classList.add('virar')
-        menuOpcoes.classList.add('aberto')
-        dropdownSelect.style.borderRadius = '3px 3px 0 0'
+    } catch (error) {
+        console.log(`Erro ao ler o arquivo: ${error}`)
     }
 }
 
-//quando clicar em um botao
-opcoes.forEach(opcao => {
-    opcao.addEventListener('click', (e) => {
-        let opcaoAtiva = document.querySelector('.ativo')
+//Abrir e fechar select
+const dropdownSelect = document.querySelector('.dropdown-select') 
+const opcaoEscolhida = document.querySelector('.selected-option')
 
-        if(opcao.dataset.value != opcaoEscolhida.dataset.value){
-            opcaoAtiva.classList.remove('ativo')
-            opcao.classList.add('ativo')
-            opcaoEscolhida.dataset.value = opcao.dataset.value
-            opcaoEscolhida.textContent = opcao.textContent
-        }
-
-        abrirOpcoes()
-    })
+dropdownSelect.addEventListener('click', (e) => {
+    mostrarOpcoesMenu()
 })
+
+//Animações de início e abertura do menu, mostrando as opções
+const opcoes = document.querySelectorAll('.opcoes')
+
+function mostrarOpcoesMenu () {
+    const seta = document.querySelector('.bxs-down-arrow')
+    const menuOpcoes = document.querySelector('.option-container')
+
+    //Se a seta estiver normal (para baixo) e o menu fechado
+    if(!seta.classList.contains('virar')) {
+        //Adiciona a classe .virar
+        seta.classList.add('virar')
+        
+        //Abre o menu de opções
+        menuOpcoes.classList.add('aberto')
+
+        //Tira o arredondamento das bordas de cima do campo de menu
+        dropdownSelect.style.borderRadius = '3px 3px 0 0'
+    } else {
+        //Tira a classe virar e adiciona a animação de desvirar
+        seta.classList.remove('virar')
+        seta.classList.add('desvirar')
+
+        //Fecha o menu de opcoes adicionando a classe .fechado
+        menuOpcoes.classList.remove('aberto')
+        menuOpcoes.classList.add('fechado')
+        dropdownSelect.style.borderRadius = '3px'
+
+        //Após o fim da animação, ele retira a classe desvirar
+        setTimeout(() => {
+            seta.classList.remove('desvirar')
+            menuOpcoes.classList.remove('fechado')
+        }, 1000);
+    }
+}
+
+function lidarComSelecaoOpcaoRoleta(opcao) {
+    //Quando clicar em uma opção do select de roletas, mostra ela como opção escolhida
+    const opcaoAtiva = document.querySelector('.ativo')
+
+    if(opcao.dataset.value != opcaoEscolhida.dataset.value){
+        opcaoAtiva.classList.remove('ativo')
+        opcao.classList.add('ativo')
+        opcaoEscolhida.dataset.value = opcao.dataset.value
+        opcaoEscolhida.textContent = opcao.textContent
+    }
+}
 
 //Criando a estrutura das opções do select, que serão chamadas ao carregar a página
 function criarEstruturaOption (regiao) {
-    let menuOpcoesSelect = document.querySelector('.outras')
-    let verificaOpcoes = menuOpcoesSelect.querySelector('.ativo')
-    let opcao = document.createElement('span')
+    const menuOpcoesSelect = document.querySelector('.option-container')
+    const verificaOpcoes = menuOpcoesSelect.querySelector('.ativo')
+    const opcao = document.createElement('span')
 
     opcao.setAttribute('data-value', regiao)
     opcao.classList.add('opcoes')
@@ -78,14 +99,17 @@ function criarEstruturaOption (regiao) {
         opcaoEscolhida.dataset.value = opcao.dataset.value
     }
 
+    opcao.addEventListener('click', e => {
+        lidarComSelecaoOpcaoRoleta(opcao)
+    })
+
     menuOpcoesSelect.appendChild(opcao)
 }
 
-
 //Menu mobile
-let botaoMenuMobile = document.querySelector('.icon-menu-mobile')
-let icon = botaoMenuMobile.querySelector('.bx')
-let navListMobile = document.querySelector('.navbar-mobile')
+const botaoMenuMobile = document.querySelector('.icon-menu-mobile')
+const icon = botaoMenuMobile.querySelector('.bx')
+const navListMobile = document.querySelector('.navbar-mobile')
 let contMenu = 0
 
 botaoMenuMobile.addEventListener('click', (e) => {
@@ -107,11 +131,11 @@ botaoMenuMobile.addEventListener('click', (e) => {
 
 //Mudar entre grade e lista
 function alterarResultado () {
-    let botaoAlterar = document.querySelector('.alterar-resultado')
-    let verifLocalStorage = localStorage.getItem('modo-resultado')
-    let icone = botaoAlterar.querySelector('.bx')
-    let listaSorteado = document.querySelector('.lista-sorteados')
-    let li = document.querySelector('.raca-vezes')
+    const botaoAlterar = document.querySelector('.alterar-resultado')
+    const verifLocalStorage = localStorage.getItem('modo-resultado')
+    const icone = botaoAlterar.querySelector('.bx')
+    const listaSorteado = document.querySelector('.lista-sorteados')
+    const li = document.querySelector('.raca-vezes')
 
     if(verifLocalStorage == 'lista' || verifLocalStorage == null) {
         //Troca icone
@@ -137,30 +161,16 @@ function alterarResultado () {
     }
 }
 
-//Começando a ler o JSON
-async function criarOpcoesSelect () {
-    try {
-        let roletas = await fetch('roletas.json')
-        let dados = await roletas.json()
-
-        for await (const objeto of dados){
-            criarEstruturaOption(objeto.regiao)
-        }
-    } catch (error) {
-        console.log(`Erro ao ler o arquivo: ${error}`)
-    }
-}
-
 //Sorteando as raças
-let botaoSortear = document.querySelector('.sortear')
+const botaoSortear = document.querySelector('.sortear')
 
 botaoSortear.addEventListener('click', (e) => {
-    let numVezes = document.getElementById('nVezes').value
+    const numVezes = document.getElementById('nVezes').value
 
     if(numVezes.length > 0 && numVezes > 0) {
         sortearRaca()
     } else {
-        let modal = document.querySelector('.modal')
+        const modal = document.querySelector('.modal')
 
         modal.classList.toggle('ativo')
         setTimeout(() => {
@@ -172,12 +182,12 @@ botaoSortear.addEventListener('click', (e) => {
 //Permite o sorteio ao apertar Enter
 document.addEventListener('keypress', e => {
     if (e.key == 'Enter') {
-        let numVezes = document.getElementById('nVezes').value
+        const numVezes = document.getElementById('nVezes').value
 
         if(numVezes.length > 0 && numVezes > 0) {
             sortearRaca()
         } else {
-            let modal = document.querySelector('.modal')
+            const modal = document.querySelector('.modal')
 
             modal.classList.toggle('ativo')
             setTimeout(() => {
@@ -190,8 +200,16 @@ document.addEventListener('keypress', e => {
 async function sortearRaca () {
     try {
         const numVezes = document.getElementById('nVezes').value
+
+        //Obtém os dados das roletas
         const response = await fetch('roletas.json')
         const dados = await response.json()
+
+        //Obtém os dados das variações
+        const response2 = await fetch('variacoes-racas.json')
+        const dadosVariacoes = await response2.json()
+
+        //Busca o índice da roleta que o usuário escolheu no select
         const index = dados.findIndex(roleta => roleta.regiao == opcaoEscolhida.dataset.value)
 
         if(index != null) {
@@ -200,14 +218,15 @@ async function sortearRaca () {
 
             //Populando o array com o nome das raças
             while(i < numVezes){
-                array[i] = acharRacaSorteada(dados[index].roletas)
+                //Envia o array da roleta e o array das variações
+                array[i] = await acharRacaSorteada(dados[index].roletas, dadosVariacoes)
                 i++
             }
 
-            //O array tem o nome das raças
+            //Com o array preenchido, envia o resultado para a função que irá mostra-lo na tela
             mostrarResultado(array)
         } else {
-            console.log('Erro')
+            console.log('Roleta inválida')
         }
         
     } catch (error) {
@@ -215,87 +234,104 @@ async function sortearRaca () {
     }
 }
 
-function acharRacaSorteada (array) {
+function acharRacaSorteada (array, dadosVariacoes) {
     let contador = 0
 
-    while (contador < array.length ) {
-        //calcula um número aleatório tendo o range máximo 0 e o tamanho da roleta do array, cada roleta pode ter números independentes e vai dar certo
-        let indiceAleatorio = Math.floor(Math.random()*array[contador].objetos.length)
-        let roletas = array[contador].objetos
+    try {
+        while (contador < array.length ) {
+            //calcula um número aleatório tendo o faixa máxima 0 e o tamanho da roleta do array, cada roleta pode ter números independentes e vai dar certo
+            const roletas = array[contador].objetos
+            const indiceAleatorio = Math.floor(Math.random()*roletas.length)
+    
+            if(roletas[indiceAleatorio].raca == `ROLETA ${contador+2}`) {
+                contador++
+            } else {
+                //Vai verificar se a raça escolhida tem variações
+                const racaEscolhida = roletas[indiceAleatorio].raca.toLowerCase()
+                const indexDaVariacaoDaRaca = dadosVariacoes.findIndex(variacao => variacao.raca.toLowerCase() == racaEscolhida)
+    
+                //Se for diferente de -1, está com o índice da variação
+                if(indexDaVariacaoDaRaca != -1){
+                    contador = 0
+    
+                    //Vai buscar até que obtenha uma raça, se cair "ROLETA X", ele faz de novo
+                    while (contador < dadosVariacoes[indexDaVariacaoDaRaca].roletas.length) {
+                        // //Gera o numero aleatório
+                        const roleta = dadosVariacoes[indexDaVariacaoDaRaca].roletas[contador]
+                        const randomIndex = Math.floor(Math.random() * roleta.objetos.length)
+    
+                        if(roleta.objetos[randomIndex].nome == `ROLETA ${contador+2}`){
+                            contador++
+                        } else {
+                            return roleta.objetos[randomIndex].nome
+                        }
+                    }
+                }
 
-        if(roletas[indiceAleatorio].raca == `ROLETA ${contador+2}`) {
-            contador++
-        } else {
-            contador += array.length
-            return roletas[indiceAleatorio].raca
+                return roletas[indiceAleatorio].raca
+            }
         }
+    } catch (error) {
+        console.log(`Erro ao buscar a raça. [ERROR]: ${error}`);
     }
 }
 
+//Constrói a estrutura que irá mostrar o resultado na tela e agrupa as raças iguais colocando o número de repetições
 function mostrarResultado (array) {
-    let resultado = document.querySelector('.resultado')
+    const resultado = document.querySelector('.resultado')
 
     //resetando o valor do resultado
     resultado.innerHTML = ''
 
     //Criando a parte de cima dos resultados
-    let divTituloBotoes = document.createElement('div')
+    const divTituloBotoes = document.createElement('div')
     divTituloBotoes.classList.add('div-titulo-botoes')
     resultado.appendChild(divTituloBotoes)
 
-    let h2Titulo = document.createElement('h2')
+    const h2Titulo = document.createElement('h2')
     h2Titulo.classList.add('titulo-resultado')
     h2Titulo.textContent = 'O destino decidiu:'
     divTituloBotoes.appendChild(h2Titulo)
     
-    let divBotoes = document.createElement('div')
+    const divBotoes = document.createElement('div')
     divBotoes.classList.add('botoes-resultado')
     divTituloBotoes.appendChild(divBotoes)
 
-    let botaoGradeLista = document.createElement('button')
+    const botaoGradeLista = document.createElement('button')
     botaoGradeLista.classList.add('alterar-resultado')
     divBotoes.appendChild(botaoGradeLista)
     botaoGradeLista.addEventListener('click', (e) => {
         alterarResultado()
     })
 
-    let iconeGradeLista = document.createElement('i')
+    const iconeGradeLista = document.createElement('i')
     iconeGradeLista.classList.add('bx')
     //O atributo de lista ou grade está mais abaixo
     botaoGradeLista.appendChild(iconeGradeLista)
 
-    let botaoCopiar = document.createElement('button')
-    botaoCopiar.classList.add('copiar')
-    divBotoes.appendChild(botaoCopiar)
-
-    let iconCopiar = document.createElement('i')
-    iconCopiar.classList.add('bx')
-    iconCopiar.classList.add('bxs-copy')
-    iconCopiar.setAttribute('aria-label', 'Copiar resultado')
-    botaoCopiar.appendChild(iconCopiar)
-
     //Lista de raças
-    let ulListaSorteado = document.createElement('ul')
+    const ulListaSorteado = document.createElement('ul')
     ulListaSorteado.classList.add('lista-sorteados')
     resultado.appendChild(ulListaSorteado)
 
-    let sorteados = verificarRepetidos(array)
+    //Agrupa os de mesma raça
+    const sorteados = verificarRepetidos(array)
 
     sorteados.forEach(raca => {
-        let liRacaVezes = document.createElement('li')
+        const liRacaVezes = document.createElement('li')
         liRacaVezes.classList.add('raca-vezes')
         ulListaSorteado.appendChild(liRacaVezes)
 
-        let pRaca = document.createElement('p')
+        const pRaca = document.createElement('p')
         pRaca.classList.add('raca')
         pRaca.textContent = raca.nome
         liRacaVezes.appendChild(pRaca)
 
-        let spanVezes = document.createElement('span')
+        const spanVezes = document.createElement('span')
         spanVezes.classList.add('vezes')
         liRacaVezes.appendChild(spanVezes)
 
-        let spanNumeroVezes = document.createElement('span')
+        const spanNumeroVezes = document.createElement('span')
         spanNumeroVezes.classList.add('numero-vezes')
         spanVezes.appendChild(spanNumeroVezes)
         spanNumeroVezes.textContent = raca.repeticoes
@@ -305,7 +341,7 @@ function mostrarResultado (array) {
 
     //Verifica a preferência do usuário
     if(localStorage.getItem('modo-resultado')){
-        let modo = localStorage.getItem('modo-resultado')
+        const modo = localStorage.getItem('modo-resultado')
 
         if(modo == 'lista') {
             iconeGradeLista.classList.add('bxs-grid-alt')
@@ -334,7 +370,8 @@ function verificarRepetidos(array) {
     const resultado = Object.keys(contagem).map((elemento) => {
         return { nome: elemento, repeticoes: contagem[elemento] };
     });
-    
+
+    //Ordena o resultado do maior ao menor
     resultado.sort((a, b) => b.repeticoes - a.repeticoes)
 
     return resultado
